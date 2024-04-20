36ba29421e3d9bd556098f067a6328930970bb61
Timofey Obraztsov <timofeyobraztsov@gmail.com>
2024-04-20 11:25:34 -0700

    route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
-import { Work_Sans } from 'next/font/google'
-import { Rubik } from 'next/font/google'
-import {Component} from "@/components/component"
+import { Work_Sans } from "next/font/google";
+import { Rubik } from "next/font/google";
+import { Component } from "@/components/component";
 
 const work_sans = Work_Sans({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-work_sans',
-})
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-work_sans",
+});
 const rubik = Rubik({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-rubik',
-})
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-rubik",
+});
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          <Component />
+      <Component />
     </main>
   );
 }
