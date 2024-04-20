48bfd1642d932b6281c54dfc74199fa722a69397
Thor Christoffersen Hochman <thorchri@usc.edu>
2024-04-20 01:51:26 -0700

    push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ const rubik = Rubik({
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
           <Component />
     </main>
   );
