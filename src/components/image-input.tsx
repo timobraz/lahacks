d54883a64834324aa0fed1f1c1e2503e87412f87
Andrew Hwang <pandy430@gmail.com>
2024-04-21 05:14:50 -0700

    yup

--- src/components/image-input.tsx
+++ src/components/image-input.tsx
@@ -5,7 +5,11 @@ import { useState, useEffect } from "react";
 import { supabase } from "@/lib/supabase";
 import { v4 as uuidv4 } from "uuid";
 
-export function ImageInput({ onUpload }: { onUpload: (images: string[]) => void }) {
+export function ImageInput({
+  onUpload,
+}: {
+  onUpload: (images: string[]) => void;
+}) {
   const [images, setImages] = useState<File[]>([]);
   const [uploadedImages, setUploadedImages] = useState<string[]>([]);
 
@@ -21,7 +25,9 @@ export function ImageInput({ onUpload }: { onUpload: (images: string[]) => void 
     for (const file of images) {
       const uniqueName = uuidv4();
       const filePath = `${uniqueName}.${file.name.split(".").pop()}`;
-      const { error } = await supabase.storage.from("images").upload(filePath, file);
+      const { error } = await supabase.storage
+        .from("images")
+        .upload(filePath, file);
 
       if (error) {
         console.error("Error uploading image:", error);
@@ -37,10 +43,17 @@ export function ImageInput({ onUpload }: { onUpload: (images: string[]) => void 
   return (
     <div className="w-full flex flex-col gap-5">
       <Label htmlFor="image" className="items-center flex justify-center">
-        Add up to 4 memorable photos
+        Add up to 4 memorable photos (the first one will be your profile
+        picture)
       </Label>
       <div className="flex justify-between w-full gap-5 items-center pt-2 ">
-        <Input accept="image/*" id="image" type="file" className="flex justify-center items-center p-3" onChange={handleImageChange} />
+        <Input
+          accept="image/*"
+          id="image"
+          type="file"
+          className="flex justify-center items-center p-3"
+          onChange={handleImageChange}
+        />
         <div
           onClick={handleSubmit}
           className="rounded-md h-12 bg-slate-700 p-5 text-white text-center items-center flex justify-center cursor-pointer"
