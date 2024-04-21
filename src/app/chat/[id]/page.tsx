f868a0d46812628b74d4e4e0ecc221733db77da5
Thor Christoffersen Hochman <thorchri@usc.edu>
2024-04-20 17:53:52 -0700

    added back arrow to chat id

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { Chat } from "@/components/chat";
+import { Back } from "@/components/back";
 
 export const Page = ({ params }: any) => {
   return (
-    <div>
+    <div className="flex bg-gray-100 dark:bg-gray-900 p-5">
+      <div className=" bg-gray-100 dark:bg-gray-900 p-5">
+        <Back before="leaderboard" />
+      </div>
       <Chat channelId={params.id} />
+      <div className=" bg-gray-100 dark:bg-gray-900 p-5 invisible">
+        <Back before="profile" />
+      </div>
     </div>
   );
 };
